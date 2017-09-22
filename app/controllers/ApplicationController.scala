23b7eac708987d69c4e621730ec57c0ff1ea51ea
katokonn <katokonn@gmail.com>
2017-09-22 12:19:50 +0900

    曜日と進行方向が反映されていなかったのを修正

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -4,36 +4,21 @@ package controllers
     import play.api.libs.json.{JsValue, Json}
     import play.api.libs.ws.{WSClient, WSResponse}
     import play.api.mvc._
-    import scala.concurrent.{Await, Future}
     import service.StationService
-    import scala.concurrent.ExecutionContext.Implicits.global
-    import play.api.libs.ws.WSResponse
     import scala.concurrent._
     import ExecutionContext.Implicits.global
-    import scala.concurrent.duration.{Duration, MILLISECONDS}
     import play.api.libs.json.Json
     import scala.sys.process._
 
     class ApplicationController @Inject()(WS: WSClient) extends Controller {
-
-      def index(time: String ,week: Int,direction:Int) = Action.async { implicit request =>
+      def index(time: String ,week: String,direction:String) = Action.async { implicit request =>
         StationService.listAllStations map { stations =>
-          val processString = s"curl -x 195.222.68.23:3128 -L https://rp.cloudrail.jp/tw02/jreast_app/fb/feedback/feedback/?accessTime=$time&accessDayCd=$week&direction=$direction"
+          val processString = s"""curl -x 115.36.15.26:3128 -L "https://rp.cloudrail.jp/tw02/jreast_app/fb/feedback/feedback/?accessTime=$time&accessDayCd=$week&direction=$direction""""
           def dirList = Process(Seq("sh", "-c", processString)).lines.mkString
-          Thread.sleep(3000)
           val trainCongestions = (Json.parse(dirList) \ "trainFeedbackList").as[List[JsValue]]
           Ok(views.html.index(trainCongestions, StationForm.form,  stations,  time, week, direction))
-          //Ok(views.html.index("te"))
+          //Ok(views.html.index("test"))
         }
       }
-      /*
-      def index(time: String ,week: Int,direction:Int) = Action.async { implicit request =>
-        StationService.listAllStations map { stations =>
-          val response: WSResponse = Await.result(WS.url(s"https://rp.cloudrail.jp/tw02/jreast_app/fb/feedback/feedback/?accessTime=$time&accessDayCd=$week&direction=$direction").get(), Duration(2000, MILLISECONDS))
-          val trainCongestions = (Json.parse(response.body) \ "trainFeedbackList").as[List[JsValue]]
-          Ok(views.html.index(trainCongestions, StationForm.form,  stations,  time, week, direction))
-        }
-      }
-      */
     }
 
